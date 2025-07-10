03a9fbc480de904091dce8bd78dc67729451db2c
Natan Ribeiro <natanribeirodeveloper@gmail.com>
2025-07-10 15:08:35 -0300

    feat(seeder): add UsersTableSeeder and update DatabaseSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => app('hash')->make('admin'),
+        ]);
+    }
+} 
